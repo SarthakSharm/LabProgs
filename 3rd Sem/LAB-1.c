2523cc43a5404ff9d5b871f63b273f5cab359cf5
sarthak070 <sarthaksharma070@gmail.com>
2021-04-09 23:00:43 +0530

    Fixed for gcc

--- 3rd Sem/LAB-1.c
+++ 3rd Sem/LAB-1.c
@@ -4,19 +4,21 @@
 
 int a[MAX],n,elem,i,pos ;
 
-void create(){
+int create(){
     printf("size: ");
     scanf("%d",&n);
     for ( i=0 ; i <n ; ++i)
         scanf("%d",&a[i]);
+    return 0;
 }
 
-void display(){
+int display(){
     for ( i = 0 ; i < n ; ++i)
         printf("%d\t",a[i]);
+return 0;
 }
 
-void insert(){
+int insert(){
     printf("Enter POS and Number :\n");
     scanf("%d%d",&pos,&elem);
     pos=pos-1;
@@ -24,9 +26,10 @@ void insert(){
         a[i+1]=a[i];
     a[pos]=elem;
     n+=1;
+    return 0;
 }
 
-void delete(){
+int deletee(){
     printf("Enter pos: ");
     scanf("%d",&pos);
     pos-=1;
@@ -34,9 +37,10 @@ void delete(){
     for(i=pos; i <= n-1 ; ++i)
         a[i]=a[i+1];
     n-=1;
+return 0;
 }
 
-void main(){
+int main(){
     int ch;
     do{
         printf("Enter choice :");
@@ -45,10 +49,11 @@ void main(){
             case 1: create(); break;
             case 2: display(); break;
             case 3: insert(); break;
-            case 4: delete(); break;
-            case 5: exit(0); break;
+            case 4: deletee(); break;
+            //case 5: exit(0); break;
             default: printf("Invalid!\n"); break;
         }
     }while(ch!=5);
     getch();
+    return 0;
 }
